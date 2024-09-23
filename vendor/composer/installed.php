9c0e3cc48e2c43abea756e6e5933996f73b91f85
Tarık KAMAT <tarikkamat2@gmail.com>
2024-09-23 18:20:23 +0300

    composer updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'iyzico/iyzipay-woocommerce',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd3d26d62df1e167c78a8816a443e376da4adbef7',
         'type' => 'woocommerce-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'iyzico/iyzipay-php' => array(
-            'pretty_version' => 'v2.0.55',
-            'version' => '2.0.55.0',
-            'reference' => '4f6abcd1f4a1ec5db3f6d840e08cd57c910aeff2',
+            'pretty_version' => 'v2.0.56',
+            'version' => '2.0.56.0',
+            'reference' => '5e4e8116429353e80da8852b31fb46ef42db47e7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../iyzico/iyzipay-php',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'iyzico/iyzipay-woocommerce' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd3d26d62df1e167c78a8816a443e376da4adbef7',
             'type' => 'woocommerce-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
